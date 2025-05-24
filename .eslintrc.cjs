a16871fd64e82d45c5e4d703b573fa1209d715fe
LuanSilvaSysmap <luan.silva@sysmap.com.br>
2025-05-24 12:57:25 -0300

    configurando projeto

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': ['warn'],
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
